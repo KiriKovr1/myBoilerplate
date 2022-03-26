b14d39432d38bafa0e41bb9735be5caebd36455f
KiriKovr1 <ivan.kirilenko.info@gmail.com>
2022-03-26 16:28:26 +0300

    front

--- client/config/Root.jsx
+++ client/config/Root.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 const Root = () => {
     return (
-        <div>this is Root</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div> this is react router v6</div>}/>
+                <Route path="/test" element={<div> this is test</div>}/>
+                <Route path="*" element={<h3>404</h3>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './assets/styles/styles.scss'
+
+import Root from './config/Root'
+
 const target = document.getElementById('root')
 
 const render = (Component) => {
